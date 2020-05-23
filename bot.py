870228ef6dba2d90a936c71a9cd61db3cda39cc4
BuildTools <discordleaf@protonmail.com>
2020-05-23 23:13:27 +0200

    initial

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import config
+from discord.http import Route
+
+class Publisher(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.channel.id in config.channels:
+            await self.http.request(
+                Route('POST', '/channels/{}/messages/{}/crosspost'.format(
+                    message.channel.id,
+                    message.id
+                )))
+
+
+bot = Publisher()
+bot.run(config.token)
